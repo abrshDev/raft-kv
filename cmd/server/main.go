26fabdc46d62462f0a236c5459b37fb3fe0b1ba4
abrshDev <abrshmelkamu3@gmail.com>
2026-07-02 17:36:04 +0300

    feat(cmd): wire up store, server, and ServeMux in main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"raft-kv/internal/kvstore"
+	"raft-kv/internal/server"
+)
+
+func main() {
+	store := kvstore.New()
+	srv := server.New(store)
+
+	mux := http.NewServeMux()
+	srv.RegisterRoutes(mux)
+
+	log.Println("listening on :9000")
+	if err := http.ListenAndServe(":9000", mux); err != nil {
+		log.Fatal(err)
+	}
+}
